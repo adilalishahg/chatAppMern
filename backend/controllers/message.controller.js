4aaa4d5065ff29338ea5c081a0e70acee3f906ce
adilalishahg <adilalishahg@gmail.com>
2024-02-15 16:43:22 -0700

    [conversation][done]

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,10 +1,38 @@
+import mongoose from "mongoose";
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
-    const { userId } = req.params;
+    const { userId: recieverId } = req.params;
+    console.log(recieverId);
     const senderId = req.user._id;
     console.log(senderId);
-    res.status(200).json({ message, userId });
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, recieverId] },
+    });
+
+    if (!conversation) {
+      console.log("convewration not found");
+      conversation = await Conversation.create({
+        participants: [senderId, recieverId],
+        messages: [],
+      });
+    }
+    const newMessage = new Message({
+      sender: senderId, // Set the sender field explicitly
+      receiver: recieverId,
+      message,
+    });
+    if (newMessage) {
+      // Save the message ID to the conversation
+      conversation.messages.push(newMessage._id);
+    }
+    await conversation.save();
+    await newMessage.save();
+    console.log(newMessage);
+    res.status(201).json(newMessage);
   } catch (error) {
     console.log(`Error in sendMessage controller ${error.message}`);
     res
--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const conversationSchema = new mongoose.Schema(
   {
-    particiants: [
+    participants: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
