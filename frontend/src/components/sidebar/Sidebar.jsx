3c5719c56874c080d058c64fc545f74a1a60ef41
adilalishahg <adilalishahg@gmail.com>
2024-02-16 16:59:30 -0700

    [Home working]

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <SearchInput />
+      <div className="divider px-3"></div>
+      {/* <Conversations />
+      <LoginButton /> */}
+    </div>
+  );
+};
+
+export default Sidebar;
