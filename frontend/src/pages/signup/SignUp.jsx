03826e363359b150e30ac823e1ca79175167fe42
adilalishahg <adilalishahg@gmail.com>
2024-02-16 16:22:54 -0700

    [Login][Designed]

---
+++ frontend/src/pages/signup/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="flex items-center justify-center min-w-96 mx-auto">
+      SignUp
+    </div>
+  );
+};
+
+export default SignUp;
